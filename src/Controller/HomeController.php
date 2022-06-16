d8e37dd1505ed08a21012f8575acc10f2405fe7f
worou <adimicool@gmail.com>
2022-06-16 12:56:25 +0200

    first commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,9 +5,33 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Psr\Log\LoggerInterface;
 
 class HomeController extends AbstractController
 {
+    private $data;
+    public function __construct()
+    {
+        $this->data =  [
+            ["id" => 1, "name" => "Dupond", "lastname" => "Roger", "age" => 22, "image" => "roger.jpg"],
+            ["id" => 2, "name" => "Durand", "lastname" => "Thomas", "age" => 52, "image" => "thomas.jpeg"],
+        ];
+    }
+
+    #[Route('/log', name: 'app_log')]
+    public function indexLogger(LoggerInterface $logger)
+    {
+        $logger->info('I just got the logger');
+        $logger->error('Erreur grave');
+
+        $logger->critical('Erreur critique', [
+            // include extra "context" info in your logs
+            'cause' => 'Erreur critique',
+        ]);
+
+        return new Response("<html><head></head><body>test</body></html>");
+    }
+    
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
@@ -19,12 +43,21 @@ class HomeController extends AbstractController
     #[Route('/person', name: 'app_person')]
     public function getData(): Response
     {
-        $data = [
-            ["id"=>1, "name"=>"Dupond", "lastname"=>"Roger","age"=>22, "image"=>"dupond.jpg"],
-            ["id"=>2, "name"=>"Durand", "lastname"=>"Simon","age"=>52, "image"=>"simon.jpg"],
-        ];
+
         return $this->render('home/data.html.twig', [
-            'personnes' => $data,
+            'personnes' => $this->data,
         ]);
+    }
+
+    /**
+     * @Route("/admin/person/{id}", name="person_item")
+     */
+    public function getItem(int $id)
+    {
+        foreach ($this->data as $person) {
+            if ($person['id'] == $id) {
+                return $this->render('home/item_person.html.twig', ['person_data' => $person]);
+            }
+        }
     }
 }
