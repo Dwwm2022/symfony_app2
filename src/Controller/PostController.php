b438af1348cce9b12dbcaad2b20fba4be6f3785a
Nicodeme <Nicodeme.Pehounde@externe.apfa.fr>
2022-06-21 14:15:25 +0200

    add edit form

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,13 +3,19 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Entity\Theme;
+use App\Form\PostType;
 use DateTimeImmutable;
 use App\Repository\PostRepository;
+use Doctrine\ORM\Mapping\Entity;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class PostController extends AbstractController
 {
@@ -24,20 +30,37 @@ class PostController extends AbstractController
     /**
      * @Route("/post", name="create_post")
      */
-    public function createPost(ManagerRegistry $doctrine){
+    public function createPost(ManagerRegistry $doctrine, Request $request){
         $manager = $doctrine->getManager();
 
         $post = new Post();
-        $post->setReference("52356totot30")
-             ->setTitle("Election législative")
-             ->setContent("survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
-             ->setAuthor("Anoine")
-             ->setPublishedAt(new \DateTimeImmutable())
-             ->setImage("election.jpg");
-        $manager->persist($post);
-        $manager->flush();
-        $this->addFlash('success', "Article N°: ".$post->getId()." est créé");
-        return $this->redirectToRoute('list_posts', ['id'=>$post->getId()]);
+        $form = $this->createFormBuilder($post)
+                     ->add('reference',null, ['label'=>'Reference', 'attr'=>['placeholder'=>'Entrez la référence']])
+                     ->add('title', TextType::class, ['label'=>'Titre', 'attr'=>['placeholder'=>'Entrez la référence']])
+                     ->add('author', null, ['label'=>'Auteur','attr'=>['placeholder'=>'Entrez l\'auteur']])
+                     ->add('theme', EntityType::class, ['class'=>Theme::class, 'choice_label'=>'name'])
+                     ->add('image')
+                     ->add('content', null, ['label'=>'Contenu','attr'=>['placeholder'=>'Entrez le contenu']])
+                     ->add('soumettre', SubmitType::class)
+                     ->getForm();
+        $form->remove('publishedAt');
+        $post->setPublishedAt(new \DatetimeImmutable());
+        $form->handleRequest($request);
+        //dd($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($post);
+            $manager->flush();
+            $this->addFlash('success', "Article N°: ".$post->getId()." est créé");
+            return $this->redirectToRoute('list_posts');
+        }
+        // $post->setReference("52356totot30")
+        //      ->setTitle("Election législative")
+        //      ->setContent("survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+        //      ->setAuthor("Anoine")
+        //      ->setPublishedAt(new \DateTimeImmutable())
+        //      ->setImage("election.jpg");
+
+        return $this->renderForm('post/add.html.twig',['form_post'=>$form]);
     }
 
     #[Route('/list', name:"list_posts")]
@@ -66,16 +89,24 @@ class PostController extends AbstractController
     #[Route('/update/{id}', name:"update_post")]
     public function updatePost(PostRepository $repo, $id, ManagerRegistry $doctrine){
         $post = $repo->find($id);
-        $post->setTitle('Nième sanction')
-            ->setAuthor('Nabil');
-        $manager = $doctrine->getManager();
-        $manager->flush();
-        $this->addFlash('success','L\'article '.$post->getTitle().' a été modifié');
-        return $this->redirectToRoute('list_posts');
+        $form = $this->createForm(PostType::class, $post);
+        $form->remove('publishedAt');
+        // $post->setTitle('Nième sanction')
+        //     ->setAuthor('Nabil');
+        // $manager = $doctrine->getManager();
+        // $manager->flush();
+        //$this->addFlash('success','L\'article '.$post->getTitle().' a été modifié');
+        //return $this->redirectToRoute('list_posts');
+        return $this->renderForm('post/edit.html.twig', ['form_edit'=>$form]);
     }
 
     #[Route('/show/{id}', name:'show_post')]
     public function showPost(Post $post){
         return $this->render('post/show.html.twig', ['post'=>$post]);
+    }
+
+    #[Route('/show/theme/{id}', name:'show_theme_id')]
+    public function showTheme(Theme $theme){
+        return $this->render('post/show_theme.html.twig', ['theme'=>$theme]);
     }
 }
